fe521ca7a8f884e3bfc0860816e81b996351e3ba
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-10-17 13:26:25 +0100

    Expose port 80 in the development environment.

--- develop.sh
+++ develop.sh
@@ -3,6 +3,7 @@
 # Set up a development environment in a container.
 
 echo docker run -i -t --rm \
+    --publish=127.0.0.1:80:80 \
     --volume=`pwd`:/src/test-baserock-infrastructure \
     baserock/openid-provider
 
