4a301cd99e15dd98e7dce07f4bdd7118f97843d7
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-10-17 15:57:59 +0100

    Note that Python 3 isn't yet supported by django_openid_provider
    Otherwise we'd be able to stop using 5 year old technology :)

--- temporary-provisioner.sh
+++ temporary-provisioner.sh
@@ -5,6 +5,9 @@
 # Puppet, since it looks like Puppet will be the quickest
 # route to getting Storyboard up ...)
 
+# I'd like to use Python 3 for this, but seems that
+# django_openid_provider needs fixing for Python 3.
+
 yum install python-pip --assumeyes
 pip install django django_openid_provider python-openid
 
