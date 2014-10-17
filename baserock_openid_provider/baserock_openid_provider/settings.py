9a4f736da5534b4e8187381ff5bcfb7ecad66501
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-10-17 15:25:52 +0100

    Add django_openid_provider to the baserock_openid_provider project
    This is added as a symlink into /usr/lib/python2.7/site-packages which
    may not be the best approach. But it keeps this repo clean, for now.

--- baserock_openid_provider/baserock_openid_provider/settings.py
+++ baserock_openid_provider/baserock_openid_provider/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'openid_provider',
 )
 
 MIDDLEWARE_CLASSES = (
--- baserock_openid_provider/baserock_openid_provider/urls.py
+++ baserock_openid_provider/baserock_openid_provider/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^openid/', include('openid_provider.urls'))
 )
