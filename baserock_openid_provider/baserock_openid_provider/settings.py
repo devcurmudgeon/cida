2aa2206eacdca7a8f3a0368a578c58aaefecb8bd
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-11-07 16:49:06 +0000

    openid_provider: Add django-registration

--- baserock_openid_provider/baserock_openid_provider/settings.py
+++ baserock_openid_provider/baserock_openid_provider/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'openid_provider',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -44,7 +45,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
--- baserock_openid_provider/baserock_openid_provider/urls.py
+++ baserock_openid_provider/baserock_openid_provider/urls.py
@@ -6,8 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'baserock_openid_provider.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^openid/', include('openid_provider.urls')),
-
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login')
 )
