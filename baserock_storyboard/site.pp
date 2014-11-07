c3c465ba2188af13a1d18075d51c81abc3d19495
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-11-07 15:29:46 +0000

    storyboard: Various changes towards getting to work
    Main obstacle now seems to be the lack of systemd in the default Docker
    container setup.

--- baserock_storyboard/site.pp
+++ baserock_storyboard/site.pp
@@ -1,6 +1,22 @@
 node default {
-    class { 'storyboard':
-        mysql_user_password => 'insecure',
-        rabbitmq_user_password => 'insecure'
-    }
+  group { 'ssl-cert':
+    ensure => 'present'
+  }
+
+  # TEMPORARY SSL private key
+  openssl::certificate::x509 { 'storyboard_dummy':
+    country => 'UK',
+    organization => 'The Baserock Project',
+    commonname => 'baserock.org',
+    base_dir => '/etc/ssl',
+    password => 'insecure'
+  } ->
+
+  class { 'storyboard':
+    mysql_user_password => 'insecure',
+    rabbitmq_user_password => 'insecure',
+    ssl_cert_file => '/etc/ssl/certs/storyboard_dummy.crt',
+    ssl_key_file => '/etc/ssl/certs/storyboard_dummy.key',
+    require => Group['ssl-cert']
+  }
 }
