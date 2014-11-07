c9c5861f4dff4e7f40486924606cf6907c846efc
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-11-07 17:46:07 +0000

    openid_provider: Add a view for /
    Previously / produced an error page, now we actually have an index page!

--- baserock_openid_provider/baserock_openid_provider/urls.py
+++ baserock_openid_provider/baserock_openid_provider/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'baserock_openid_provider.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
 
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
