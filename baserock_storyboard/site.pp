0d76eca6351002abd76aae29d8734c354f2b9685
Sam Thursfield <sam.thursfield@codethink.co.uk>
2014-12-05 19:59:53 +0000

    storyboard: Work in progress on production deployment

--- baserock_storyboard/site.pp
+++ baserock_storyboard/site.pp
@@ -3,20 +3,44 @@ node default {
     ensure => 'present'
   }
 
+  # This directory doesn't seem to exist by default in Fedora
+  file { '/etc/ssl/private':
+    ensure => directory
+    before => Class['storyboard::cert']
+  }
+
   # TEMPORARY SSL private key
   openssl::certificate::x509 { 'storyboard_dummy':
     country => 'UK',
     organization => 'The Baserock Project',
     commonname => 'baserock.org',
-    base_dir => '/etc/ssl',
-    password => 'insecure'
-  } ->
+    base_dir => '/tmp/',
+    password => 'insecure',
+    before => Class['storyboard::cert']
+  }
 
-  class { 'storyboard':
-    mysql_user_password => 'insecure',
-    rabbitmq_user_password => 'insecure',
-    ssl_cert_file => '/etc/ssl/certs/storyboard_dummy.crt',
-    ssl_key_file => '/etc/ssl/certs/storyboard_dummy.key',
-    require => Group['ssl-cert']
+  class { 'storyboard::cert':
+    ssl_cert_file => '/tmp/storyboard_dummy.crt',
+    ssl_key_file => '/tmp/storyboard_dummy.key',
+    ssl_ca_file => '/etc/ssl/certs/ca-bundle.crt'
+  }
+
+  # need class storyboard::rabbitmq too
+
+  class { 'storyboard::application':
+    openid_url => 'http://openid.baserock.org/',
+
+    mysql_host => '192.168.222.30',
+    mysql_database => 'storyboard',
+    mysql_user => 'storyboard',
+    # FIXME: need to read this from a file in /var/lib
+    mysql_user_password => 'storyboard_insecure',
+
+    rabbitmq_host => 'localhost',
+    rabbitmq_port => 5672,
+    rabbitmq_vhost => '/',
+    rabbitmq_user => 'storyboard',
+    # FIXME: need to read this from a file in /var/lib
+    rabbitmq_user_password => 'storyboard_insecure'
   }
 }
