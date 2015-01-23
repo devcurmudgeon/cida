7a1db64930b2f5373cb7145783cb6afd17acd855
Sam Thursfield <sam.thursfield@codethink.co.uk>
2015-01-23 19:04:45 +0000

    openid_provider: Require user to provide a full name on registration
    This is needed for Storyboard to be able to use the OpenID provider: it
    uses the openid.fullname property as the user's display name.

--- baserock_openid_provider/baserock_openid_provider/views.py
+++ baserock_openid_provider/baserock_openid_provider/views.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014  Codethink Limited
+# Copyright (C) 2015  Codethink Limited
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,8 +14,42 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
+import registration.backends.simple.views
+
+from registration import signals
+from registration.users import UserModel
+
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.shortcuts import render
+
+from . import forms
 
 
 def index(request):
     return render(request, '../templates/index.html')
+
+
+class RegistrationViewWithNames(registration.backends.simple.views.RegistrationView):
+    # Overrides the django-registration default view so that the extended form
+    # including the full name gets used.
+    form_class = forms.RegistrationFormWithNames
+
+    def register(self, request, **cleaned_data):
+        # It's a shame that we have to override the whole class here. We could
+        # patch django-registration(-redux) to avoid the need.
+        username, email, password = cleaned_data['username'], cleaned_data['email'], cleaned_data['password1']
+        first_name, last_name = cleaned_data['first_name'], cleaned_data['last_name']
+        UserModel().objects.create_user(username, email, password,
+                                        first_name=first_name,
+                                        last_name=last_name)
+
+        new_user = authenticate(username=username, password=password)
+        login(request, new_user)
+        signals.user_registered.send(sender=self.__class__,
+                                     user=new_user,
+                                     request=request)
+        return new_user
+
+
+registration.backends.simple.views.RegistrationView = RegistrationViewWithNames
